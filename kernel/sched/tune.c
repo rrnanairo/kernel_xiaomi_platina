404e06a45bd23c3093aa8edcff83c942dec2b371
rrnanairo <alandika009@gmail.com>
2022-01-11 12:54:42 +0000

    sched/tune: fix value

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -836,8 +836,6 @@ schedtune_boostgroup_init(struct schedtune *st, int idx)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	boost_slots_init(st);
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
-
-	return 0;
 }
 
 static struct cgroup_subsys_state *
